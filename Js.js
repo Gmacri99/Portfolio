9a6590ee662a675f3bb9f8e45db46e4c443a9d8e
Gmacri25 <macrigianny2021@gmail.com>
2023-07-26 10:43:52 -0400

    changes

--- Js.js
+++ Js.js
@@ -1,42 +1,5 @@
 let tl = gsap.timeline()
-
-
-tl.from('.logo-header',{
-    x:-300,
-    duration: 2
-})
-
-tl.from('.li-about',{
-    opacity:0,
-    duration: 0.4
-})
-
-tl.from('.li-services',{
-    opacity:0,
-    duration: 0.35
-})
-
-tl.from('.li-skills',{
-    opacity:0,
-    duration: 0.3
-})
-
-tl.from('.li-experience',{
-    opacity:0,
-    duration: 0.25
-})
-
-tl.from('.li-contact',{
-    opacity:0,
-    duration: 0.2
-})
-
-tl.from('.about-container',{
-    y:-100,
-    opacity:0,
-    duration: 1.2
-})
-
+let match=gsap.matchMedia()
 let write=document.querySelector('.p-writting')
 
 let writting= str =>{
@@ -52,6 +15,44 @@ let writting= str =>{
     },50)
 }
 
+match.add("(min-width:768px)", ()=>{
+
+    tl.from('.logo-header',{
+        x:-300,
+        duration: 2
+    })
+    
+    tl.from('.li-about',{
+        opacity:0,
+        duration: 0.4
+    })
+    
+    tl.from('.li-services',{
+        opacity:0,
+        duration: 0.35
+    })
+    
+    tl.from('.li-skills',{
+        opacity:0,
+        duration: 0.3
+    })
+    
+    tl.from('.li-experience',{
+        opacity:0,
+        duration: 0.25
+    })
+    
+    tl.from('.li-contact',{
+        opacity:0,
+        duration: 0.2
+    })
+    
+    tl.from('.about-container',{
+        y:-100,
+        opacity:0,
+        duration: 1.2
+    })
+
 tl.call(setTimeout(()=>{writting("My name is Gianny Macri, a front end developer with experience creating websites and interactive applications. I love designing and developing webs that surprise and delight users. ")},5000))
 
 
@@ -60,3 +61,35 @@ tl.from('.about-container-div-2',{
     x:50,
     duration: 1
 })
+
+})
+
+match.add("(max-width:767px)", ()=>{
+
+    tl.from('.logo-header',{
+        opacity:0,
+        duration: 1.5
+    })
+   
+    tl.from('.sizes',{
+        opacity:0,
+        rotate:180,
+        duration: 0.8
+    })
+    
+    tl.from('.about-container',{
+        x:-400,
+        duration: 1.2
+    })
+
+tl.call(setTimeout(()=>{writting("My name is Gianny Macri, a front end developer with experience creating websites and interactive applications. I love designing and developing webs that surprise and delight users. ")},4000))
+
+
+tl.from('.about-container-div-2',{
+    opacity:0,
+    x:50,
+    duration: 1
+})
+
+})
+
